8810fc7ed974e92355f94dedcf5162b35d210f86
Samuel Khoo <115562183+SamuelKhoo2003@users.noreply.github.com>
2024-09-19 14:51:24 +0100

    chore: disable es-lint on animatedButton

--- src/components/ui/animatedButton.tsx
+++ src/components/ui/animatedButton.tsx
@@ -1,3 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import React from "react";
 import {
